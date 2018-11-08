2595ce9cfe2067ef17ac563b875682041e77c4d3
Roderick <roderick.montague@uoit.net>
2018-11-08 13:12:50 -0500

    Developer Credits

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 /*
-Developers:
-Date:
+Developers: Alexander Da Costa (100704694), Caleb Birnie (100699828), Carter Menary (100700587), Devin Fitzpatrick (100709082), Nathan Tuck (100708651), Roderick “R.J.” Montague (100701758)
+Date: 11/08/2018
 References:
 */
+
+// Game Development Workshop - Project 3 | Faster Than Light
+#include <iostream>
 
 int main()
 {
