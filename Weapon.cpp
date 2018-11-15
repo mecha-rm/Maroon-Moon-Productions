684ff0695eefdb672fac7686dac1e4710888f877
DevFitzy <devin.fitzpatrick@uoit.net>
2018-11-15 13:22:11 -0500

    Updates To Ship and Weapon Classes

--- Weapon.cpp
+++ Weapon.cpp
@@ -1,1 +1,29 @@
-// cpp
+#include <iostream>
+#include "Weapon.h"
+
+//Setters ------------------------------------------------------------------------------------------------------------
+void Weapon::setDamage(int damage) {
+	damage = Weapon::damage;
+	
+}
+void Weapon::setHealth(int health) {
+	health = Weapon::health;
+
+}
+void Weapon::setCooldown(int cooldown) {
+	cooldown = Weapon::cooldown;
+
+}
+//Getters ------------------------------------------------------------------------------------------------------------
+int Weapon::getDamage() {
+
+	return damage;
+}
+int Weapon::getHealth() {
+
+	return health;
+}
+int Weapon::getCooldown() {
+
+	return cooldown;
+}
--- Weapon.h
+++ Weapon.h
@@ -3,14 +3,21 @@
 #include <string>
 using namespace std;
 
-class Weapons {
+class Weapon {
 public:
+
+	int getDamage();
+	int getHealth();
+	int getCooldown();
+
+	void setDamage(int);
+	void setHealth(int);
+	void setCooldown(int);
+
+private:
 
 	int damage;
 	int health;
 	int cooldown;
-
-private:
-
 
 };
