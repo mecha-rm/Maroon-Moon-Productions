8e8d2e4e2dce6ef6b96fc216f10ea73607f06568
roderick-rj <43918914+roderick-rj@users.noreply.github.com>
2018-11-15 19:38:30 -0500

    Miscellaneous classes for the project.

--- Ship.cpp
+++ Ship.cpp
@@ -1,3 +1,143 @@
 #include <iostream>
+#include "Ship.h"
 using namespace std;
 
+//Setters -------------------------------------------------------------------------------------------------------------------------------------------
+void Ship::setHull(int hull) {
+	hull = Ship::hull;
+
+}
+void Ship::setShield(int shield) {
+	shield = Ship::shield;
+
+}
+void Ship::setEvade(int evade) {
+	evade = Ship::evade;
+
+}
+void Ship::setOxygen(int oxygen) {
+	oxygen = Ship::oxygen;
+
+}
+void Ship::setCrewMembers(int crewMembers) {
+	crewMembers = Ship::crewMembers;
+
+}
+void Ship::setScrap(int scrap) {
+	scrap = Ship::scrap;
+
+}
+void Ship::setFTLDrive(int ftlDrive) {
+	ftlDrive = Ship::ftlDrive;
+
+}
+void Ship::setFuel(int fuel) {
+	fuel = Ship::fuel;
+
+}
+void Ship::setShieldLevel(int shieldLevel) {
+	shieldLevel = Ship::shieldLevel;
+
+}
+void Ship::setEngineLevel(int engineLevel) {
+	engineLevel = Ship::engineLevel;
+
+}
+void Ship::setOxygenLevel(int oxygenLevel) {
+	oxygenLevel = Ship::oxygenLevel;
+
+}
+void Ship::setWeaponControlLevel(int weaponControlLevel) {
+	weaponControlLevel = Ship::weaponControlLevel;
+
+}
+void Ship::setMedbayLevel(int medbayLevel) {
+	medbayLevel = Ship::medbayLevel;
+
+}
+void Ship::setPilotLevel(int pilotLevel) {
+	pilotLevel = Ship::pilotLevel;
+
+}
+void Ship::setSensorLevel(int sensorLevel) {
+	sensorLevel = Ship::sensorLevel;
+
+}
+void Ship::setDoorLevel(int doorLevel) {
+	doorLevel = Ship::doorLevel;
+
+}
+void Ship::setRooms(int rooms) {
+	rooms = Ship::rooms;
+
+}
+//Getters -------------------------------------------------------------------------------------------------------------------------------------------
+int Ship::getHull() {
+
+	return hull;
+}
+int Ship::getShield() {
+
+	return shield;
+}
+int Ship::getEvade() {
+
+	return evade;
+}
+int Ship::getOxygen() {
+
+	return oxygen;
+}
+int Ship::getCrewMembers() {
+
+	return crewMembers;
+}
+int Ship::getScrap() {
+
+	return scrap;
+}
+int Ship::getFTLDrive() {
+
+	return ftlDrive;
+}
+int Ship::getFuel() {
+
+	return fuel;
+}
+int Ship::getShieldLevel() {
+
+	return shieldLevel;
+}
+int Ship::getEngineLevel() {
+
+	return engineLevel;
+}
+int Ship::getOxygenLevel() {
+
+	return oxygenLevel;
+}
+int Ship::getWeaponControlLevel() {
+
+	return weaponControlLevel;
+}
+int Ship::getMedbayLevel() {
+
+	return medbayLevel;
+}
+int Ship::getPilotLevel() {
+
+	return pilotLevel;
+}
+int Ship::getSensorLevel() {
+
+	return sensorLevel;
+}
+int Ship::getDoorLevel() {
+
+	return doorLevel;
+}
+
+int Ship::getRooms() {
+
+	return rooms;
+}
--- Ship.h
+++ Ship.h
@@ -8,6 +8,42 @@ public:
 
 	string shipName;
 
+	int getHull();
+	int getShield();
+	int getEvade();
+	int getOxygen();
+	int getCrewMembers();
+	int getScrap();
+	int getFTLDrive();
+	int getFuel();
+	int getShieldLevel();
+	int getEngineLevel();
+	int getOxygenLevel();
+	int getWeaponControlLevel();
+	int getMedbayLevel();
+	int getPilotLevel();
+	int getSensorLevel();
+	int getDoorLevel();
+	int getRooms();
+
+	void setHull(int);
+	void setShield(int);
+	void setEvade(int);
+	void setOxygen(int);
+	void setCrewMembers(int);
+	void setScrap(int);
+	void setFTLDrive(int);
+	void setFuel(int);
+	void setShieldLevel(int);
+	void setEngineLevel(int);
+	void setOxygenLevel(int);
+	void setWeaponControlLevel(int);
+	void setMedbayLevel(int);
+	void setPilotLevel(int);
+	void setSensorLevel(int);
+	void setDoorLevel(int);
+	void setRooms(int);
+
 private:
 
 	//resources
@@ -19,6 +55,7 @@ private:
 	int scrap;
 	int ftlDrive;
 	int fuel;
+	int rooms;
 
 	//systems
 	int shieldLevel;
