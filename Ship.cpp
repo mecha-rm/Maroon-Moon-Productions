5ba43002b01994c6f29b9ffcb705cbab04e0c281
DevFitzy <devin.fitzpatrick@uoit.net>
2018-11-19 11:45:54 -0500

    House Cleaning

--- Ship.cpp
+++ Ship.cpp
@@ -22,127 +22,47 @@ Ship::Ship(std::string name) : shipName(name)
 	setEngineLevel(2);
 	setFuel(16);
 
-	
-
-	
 }
 
-//Setters -------------------------------------------------------------------------------------------------------------------------------------------
-void Ship::setHull(int hull) {
-	this->hull = hull;
-}
-
-void Ship::setShield(int shield) {
-	this->shield = shield;
-}
-
-void Ship::setReactor(int reactor)
-{
-	this->reactor = reactor;
-}
-
-void Ship::setEvade(int evade) {
-	this->evade = evade;
-
-}
-
-void Ship::setOxygen(int oxygen) {
-	this->oxygen = oxygen;
-
-}
-
-//void Ship::setCrewMembers(int crewMembers) {
-//	this->crewMembers = crewMembers;
-//}
-
-void Ship::setScrap(int scrap) {
-	this->scrap = scrap;
-}
-
-void Ship::setFTLDrive(int ftlDrive) {
-	this->ftlDrive = ftlDrive;
-}
-
-void Ship::setFuel(int fuel) {
-	this->fuel = fuel;
-}
-
-void Ship::setShieldLevel(int shieldLevel) {
-	this->shieldLevel = shieldLevel;
-}
-
-void Ship::setEngineLevel(int engineLevel) {
-	this->engineLevel = engineLevel;
-
-}
-
-void Ship::setOxygenLevel(int oxygenLevel) {
-	
-	this->oxygenLevel = oxygenLevel;
-}
-
-void Ship::setWeaponControlLevel(int weaponControlLevel) {
-	this->weaponControlLevel = weaponControlLevel;
-}
-
-void Ship::setMedbayLevel(int medbayLevel) {
-	this->medbayLevel = medbayLevel;
-}
-
-void Ship::setPilotLevel(int pilotLevel) {
-	this->pilotLevel = pilotLevel;
-}
-
-void Ship::setSensorLevel(int sensorLevel) {
-	this->sensorLevel = sensorLevel;
-
-}
-
-void Ship::setDoorLevel(int doorLevel) {
-	this->doorLevel = doorLevel;
-}
-
-void Ship::setRooms(int rooms) {
-	this->rooms = rooms;
-}
-
+//Setters 
+void Ship::setHull(int hull) { this->hull = hull; }
+void Ship::setShield(int shield) { this->shield = shield; }
+void Ship::setReactor(int reactor) { this->reactor = reactor; }
+void Ship::setEvade(int evade) { this->evade = evade; }
+void Ship::setOxygen(int oxygen) { this->oxygen = oxygen; }
+//void Ship::setCrewMembers(int crewMembers) { this->crewMembers = crewMembers; }
+void Ship::setScrap(int scrap) { this->scrap = scrap; }
+void Ship::setFTLDrive(int ftlDrive) { this->ftlDrive = ftlDrive; }
+void Ship::setFuel(int fuel) { this->fuel = fuel; }
+void Ship::setShieldLevel(int shieldLevel) { this->shieldLevel = shieldLevel; }
+void Ship::setEngineLevel(int engineLevel) { this->engineLevel = engineLevel; }
+void Ship::setOxygenLevel(int oxygenLevel) { this->oxygenLevel = oxygenLevel; }
+void Ship::setWeaponControlLevel(int weaponControlLevel) { this->weaponControlLevel = weaponControlLevel; }
+void Ship::setMedbayLevel(int medbayLevel) { this->medbayLevel = medbayLevel; }
+void Ship::setPilotLevel(int pilotLevel) { this->pilotLevel = pilotLevel; }
+void Ship::setSensorLevel(int sensorLevel) { this->sensorLevel = sensorLevel; }
+void Ship::setDoorLevel(int doorLevel) { this->doorLevel = doorLevel; }
+void Ship::setRooms(int rooms) { this->rooms = rooms; }
 void Ship::setCrew(std::vector<CrewMember> crew) { this->crew = crew; }
 
-//Getters -------------------------------------------------------------------------------------------------------------------------------------------
+//Getters
 int Ship::getHull() { return hull; }
-
 int Ship::getShield() { return shield; }
-
 int Ship::getReactor() { return reactor; }
-
 int Ship::getEvade() { return evade; }
-
 int Ship::getOxygen() { return oxygen; }
-
 int Ship::getCrewMembers() { return crewMembers; }
-
 int Ship::getScrap() { return scrap; }
-
 int Ship::getFTLDrive() { return ftlDrive; }
-
 int Ship::getFuel() { return fuel; }
-
 int Ship::getShieldLevel() { return shieldLevel; }
-
 int Ship::getEngineLevel() { return engineLevel; }
-
 int Ship::getOxygenLevel() { return oxygenLevel; }
-
 int Ship::getWeaponControlLevel() { return weaponControlLevel; }
-
 int Ship::getMedbayLevel() { return medbayLevel; }
-
 int Ship::getPilotLevel() { return pilotLevel; }
-
 int Ship::getSensorLevel() { return sensorLevel; }
-
 int Ship::getDoorLevel() { return doorLevel; }
-
 int Ship::getRooms() { return rooms; }
 
 std::vector<CrewMember> Ship::getCrew() { return crew; }
--- Weapon.cpp
+++ Weapon.cpp
@@ -4,31 +4,21 @@
 // Creates the weapon
 Weapon::Weapon(std::string name, std::string type, int damage, int ammo, int chargeTime) : name(name), type(type), damage(damage), ammo(ammo), chargeTime(chargeTime) {}
 
-int Weapon::getDamage() {
 
-	return damage;
-}
-
+//Setters
 void Weapon::setDamage(int damage) {
 	this->damage = damage;
 }
-
-int Weapon::getAmmo() {
-
-	return ammo;
+void Weapon::setAmmo(int ammo) {
+	this->ammo = ammo;
 }
-
-void Weapon::setAmmo(int health) {
-	this->ammo = health;
-}
-
-int Weapon::getCharge() {
-	return charge;
-}
-
 void Weapon::setCharge(int charge) {
 	this->charge = charge;
 }
 
+//Getters
+int Weapon::getDamage() { return damage; }
+int Weapon::getAmmo() {	return ammo; }
+int Weapon::getCharge() { return charge; }
 int Weapon::getChargeTime() { return chargeTime; }
 
