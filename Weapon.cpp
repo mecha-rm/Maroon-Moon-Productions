6bf2c5adc1c0453b151e1d1c3bd6d48f6eba6491
DevFitzy <devin.fitzpatrick@uoit.net>
2018-11-19 15:22:03 -0500

    Types of Damage
    Added different types of damage (missile, lazer) and how they interact with the shield and hull health along with the crew member's health

--- Weapon.cpp
+++ Weapon.cpp
@@ -4,15 +4,17 @@
 // Creates the weapon
 Weapon::Weapon(std::string name, std::string type, int damage, int ammo, int chargeTime) : name(name), type(type), damage(damage), ammo(ammo), chargeTime(chargeTime) {}
 
-
 //Setters
 void Weapon::setDamage(int damage) { this->damage = damage; }
 void Weapon::setAmmo(int ammo) { this->ammo = ammo; }
 void Weapon::setCharge(int charge) { this->charge = charge; }
+void Weapon::setDamageType(std::string damageType) { this->damageType = damageType; }
+void Weapon::setRoomHit(int room) { this->room = room; }
 
 //Getters
 int Weapon::getDamage() { return damage; }
 int Weapon::getAmmo() {	return ammo; }
 int Weapon::getCharge() { return charge; }
 int Weapon::getChargeTime() { return chargeTime; }
-
+std::string Weapon::getDamageType() { return damageType; }
+int Weapon::getRoomHit() { return room; }
--- Weapon.h
+++ Weapon.h
@@ -12,16 +12,19 @@ public:
 	int getAmmo();
 	int getCharge();
 	int getChargeTime();
+	std::string getDamageType();	
+	int getRoomHit();
 
 	void setDamage(int);
 	void setAmmo(int);
 	void setCharge(int);
-	
-
+	void setDamageType(std::string);
+	void setRoomHit(int);
 
 private:
 	const std::string type;
 	const std::string name;
+	std::string damageType;
 
 	// the amount of damage a weapon does
 	int damage = 1;
@@ -30,5 +33,5 @@ private:
 	// the amount of charge the weapon currently has
 	int charge = 0;
 	const unsigned int chargeTime = 0; // the amount of time the weapon takes to charge
-
+	int room;
 };
